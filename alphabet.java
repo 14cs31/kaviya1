885e98c681f5c41d199cd16d328b74fa5a4f5f9b
14cs31 <kaviyaganesan22@gmail.com>
2016-12-23 16:23:02 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class JavaProgram
+{
+public static void main(String args[])
+charch;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter a Character :");
+ch=scan.next().charAt(0);
+if((ch>='a' && ch<='z')
+{
+System.out.println(ch + "is an Alphabet");
+}
+else
+{
+System.out.println(ch + "is Not an Alphabet");
+}
+}
+}
